5db8e75e69bc91e73747b91323ce8842f0e5ed31
vkshay3400 <akshayp2208@gmail.com>
2020-02-15 13:07:21 +0530

    Added: Employee wage as per working hours

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -7,28 +7,35 @@ IS_PART_TIME=1
 IS_FULL_TIME=2
 EMP_RATE_PER_HOUR=20
 WORKING_DAYS=20
+WORKING_TOTAL_HOURS=100
+
+# VARIABLES
 totalSalary=0
+totalDays=0
+totalEmpHours=0
 
-# GENERATE RANDOM NUMBER
-randomCheck=$((RANDOM%3))
-
-# WAGE FOR MONTH
-for (( days=1; days<=$WORKING_DAYS; days++ ))
+# OPERATION ON EMPLOYEE WAGES
+while [[ $totalEmpHours -lt $WORKING_TOTAL_HOURS && $totalDays -lt $WORKING_DAYS ]]
 do
-case $randomCheck in
+	((totalDays++))
+	
+	# GENERATE RANDOM NUMBER
+	randomCheck=$((RANDOM%3))
+	
+	case $randomCheck in
 	$IS_PART_TIME)
 		empHours=4
 		;;
 	$IS_FULL_TIME)
 		empHours=8
 		;;
-		*)
+	*)
 		empHours=0
 		;;
-esac
-	salary=$(($empHours*$EMP_RATE_PER_HOUR))
-	totalSalary=$(($totalSalary+$salary))
+	esac
+	totalEmpHours=$(( $totalEmpHours+$empHours ))
+	totalSalary=$(( $totalEmpHours*$EMP_RATE_PER_HOUR ))
 done
 
-# PRINT TOTAL SALARY
+# PRINT SALARY
 echo $totalSalary
