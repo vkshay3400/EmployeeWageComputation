beb217f57585271b143c3e3b1ef897001c150df5
vkshay3400 <akshayp2208@gmail.com>
2020-02-15 12:32:30 +0530

    Added: Daily Employee Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,13 +1,26 @@
 #!/bin/bash -x
 
-# WELCOME TO EMPLOYEE WAGE COMPUTATION
+echo "*************************WELCOME TO EMPLOYEE WAGE COMPUTATION*************************"
+
+#CONSTANTS
+EMP_RATE_PER_HOUR=20
+
+# VARIABLES
 isPresent=1
+
+# GENERATE RANDOM NUMBER
 randomCheck=$((RANDOM%2))
 
+# TO CHECK SALARY IF PRESESNT/ABSENT
 if [ $isPresent -eq $randomCheck ]
 then
 	echo "Emplpoyee is Present"
+	empHours=8
 else
 	echo "Employee is Absent"
+	empHours=0
 fi
 
+# TO GET SALARY
+salary=$(($empHours*$EMP_RATE_PER_HOUR))
+echo $salary
